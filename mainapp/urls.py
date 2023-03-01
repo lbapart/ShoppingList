3d542ee6bb7affe96666dcbd91fa5b610ffbd56b
Артем Апенко <lbapart@MacBook-Pro.local>
2023-03-01 21:21:41 +0100

    Added detailed view for item

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ShoppingList.as_view(), name='purchase')
+    path('', views.ShoppingList.as_view(), name='purchase'),
+    path('purchase/<int:pk>', views.ItemDetailed.as_view(), name='purchase-detailed')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Purchase
 
 class ShoppingList(ListView):
     model = Purchase
+
+class ItemDetailed(DetailView):
+    model = Purchase
+    template_name = 'mainapp/pur'
+
     
