778f08760faac29330a9e3197ee96da8741ab336
Артем Апенко <lbapart@MacBook-Pro.local>
2023-03-01 18:44:19 +0100

    Added first page with things to purchase

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Purchase
 
-def home(request):
-    pass
+class ShoppingList(ListView):
+    model = Purchase
+    
