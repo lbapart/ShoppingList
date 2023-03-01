94eeee04531939dee704ab2368ff53c2ec586352
Артем Апенко <lbapart@MacBook-Pro.local>
2023-03-01 22:04:56 +0100

    Added create item form

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ShoppingList.as_view(), name='purchase'),
-    path('purchase/<int:pk>', views.ItemDetailed.as_view(), name='purchase-detailed')
+    path('', views.ShoppingList.as_view(), name='items'),
+    path('item/<int:pk>/', views.ItemDetailed.as_view(), name='item-detailed'),
+    path('item-create/', views.ItemCreate.as_view(), name='item-create'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Purchase
+from django.urls import reverse_lazy
 
 class ShoppingList(ListView):
     model = Purchase
 
 class ItemDetailed(DetailView):
     model = Purchase
-    template_name = 'mainapp/pur'
+    template_name = 'mainapp/purchase_detail.html'
 
-    
+class ItemCreate(CreateView):
+    model = Purchase
+    fields = ['title', 'quantity', 'quantityType']
+    success_url = reverse_lazy('items')
